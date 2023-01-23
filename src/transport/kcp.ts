607fe46afe579c4412cf9973ddedfea39ac4bb31
Wostafa <mostaphaaa@gmail.com>
2023-01-23 17:16:37 +0330

    comments translated

--- src/transport/kcp.ts
+++ src/transport/kcp.ts
@@ -1,45 +1,45 @@
 import { HEADER_OBJECT } from "../../lib";
 
-/** 
- * mKCP 使用 UDP 来模拟 TCP 连接，请确定主机上的防火墙配置正确
- * 
- * mKCP 牺牲带宽来降低延迟。传输同样的内容，mKCP 一般比 TCP 消耗更多的流量
- */
+/**
+* mKCP uses UDP to simulate TCP connection, please make sure the firewall configuration on the host is correct
+*
+* mKCP sacrifices bandwidth to reduce latency. To transmit the same content, mKCP generally consumes more traffic than TCP
+*/
 class KcpObject {
     /**
-     * 最大传输单元（maximum transmission unit）
-     * 
-     * 请选择一个介于 576 - 1460 之间的值
+     * Maximum transmission unit (maximum transmission unit)
+     *
+     * Please select a value between 576 - 1460
      */
-    mtu: number = 1350;
+    person : number  =  1350 ;
 
     /**
-     * 传输时间间隔（transmission time interval），单位毫秒（ms），mKCP 将以这个时间频率发送数据
-     * 
-     * 请选译一个介于 10 - 100 之间的值
+     * Transmission time interval (transmission time interval), in milliseconds (ms), mKCP will send data at this time frequency
+     *
+     * Please select a value between 10 - 100
      */
     tti: number = 50;
 
-    /** 上行链路容量，即主机发出数据所用的最大带宽，单位 MB/s */
+    /** Uplink capacity, that is, the maximum bandwidth used by the host to send data, in MB/s */
     uplinkCapacity: number = 5;
 
-    /** 行链路容量，即主机接收数据所用的最大带宽，单位 MB/s */
+    /** Link capacity, that is, the maximum bandwidth used by the host to receive data, in MB/s */
     downlinkCapacity: number = 20;
 
     /**
-     * 是否启用拥塞控制 
-     * 
-     * 开启拥塞控制之后，V2Ray 会自动监测网络质量，当丢包严重时，会自动降低吞吐量；当网络畅通时，也会适当增加吞吐量
+     * Whether to enable congestion control
+     *
+     * After the congestion control is enabled, V2Ray will automatically monitor the network quality, and when the packet loss is serious, it will automatically reduce the throughput; when the network is smooth, it will also increase the throughput appropriately
      */
     congestion: boolean = false;
 
-    /** 单个连接的读取缓冲区大小，单位是 MB */
+    /** The read buffer size of a single connection, in MB */
     readBufferSize: number = 2;
 
-    /** 单个连接的写入缓冲区大小，单位是 MB */
+    /** The write buffer size of a single connection, in MB */
     writeBufferSize: number = 2;
 
-    /** 数据包头部伪装设置 */
+    /** Packet header masquerading settings */
     header: { type: HEADER_OBJECT } = { type: HEADER_OBJECT.none };
 }
 
