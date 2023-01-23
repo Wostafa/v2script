f0e195008f427a1b3322fdc2181e6d392a82326c
Wostafa <mostaphaaa@gmail.com>
2023-01-23 17:04:21 +0330

    comments translated

--- src/protocol/dns.ts
+++ src/protocol/dns.ts
@@ -1,34 +1,34 @@
 /**
- * DNS 流量的传输层协议
- */
+* Transport layer protocol for DNS traffic
+*/
 const enum DNS_NETWORK {
     tcp = "tcp",
     udp = "udp"
 }
 
 /**
- * DNS 是一个出站协议，主要用于拦截和转发 DNS 查询
- * 
- * 此出站协议只能接收 DNS 流量（包含基于 UDP 和 TCP 协议的查询），其它类型的流量会导致错误
- * 
- * 在处理 DNS 查询时，此出站协议会将 IP 查询（即 A 和 AAAA）转发给内置的 DNS 服务器，
- * 其它类型的查询流量将被转发至它们原本的目标地址
- */
+* DNS is an outbound protocol primarily used to intercept and forward DNS queries
+*
+* This outbound protocol can only receive DNS traffic (including queries based on UDP and TCP protocols), other types of traffic will cause errors
+*
+* When processing DNS queries, this outbound protocol forwards IP queries (i.e. A and AAAA) to the built-in DNS server,
+* Other types of query traffic will be forwarded to their original destination address
+*/
 class DnsOutboundObject {
-    /** 修改 DNS 流量的传输层协议，可选的值有 `tcp` 和 `udp`。当不指定时，保持来源的传输方式不变 */
+    /** Modify the transport layer protocol of DNS traffic, the optional values ​​are `tcp` and `udp`. When not specified, keep the transmission mode of the source unchanged*/
     network: DNS_NETWORK;
 
-    /** 修改 DNS 服务器地址。当不指定时，保持来源中指定的地址不变 */
+    /** Modify DNS server address. When not specified, keep the address specified in the source unchanged */
     address: string;
 
-    /** 修改 DNS 服务器端口。当不指定时，保持来源中指定的端口不变 */
+    /** Modify the DNS server port. When not specified, keep the port specified in the source unchanged */
     port: number;
 
     /**
      * DnsOutboundObject
-     * @param network 修改 DNS 流量的传输层协议
-     * @param address 修改 DNS 服务器地址
-     * @param port 修改 DNS 服务器端口
+     * @param network Modify the transport layer protocol of DNS traffic
+     * @param address Modify DNS server address
+     * @param port Modify the DNS server port
      */
     constructor(network: DNS_NETWORK, address: string, port: number) {
         this.network = network;
