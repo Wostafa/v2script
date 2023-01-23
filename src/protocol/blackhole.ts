54ca73563dc5fa02f206597b284b651d6b5a2e71
Wostafa <mostaphaaa@gmail.com>
2023-01-23 17:03:36 +0330

    comments translated

--- src/protocol/blackhole.ts
+++ src/protocol/blackhole.ts
@@ -1,23 +1,23 @@
 /**
- * * 当 type 为 "none"（默认值）时，Blackhole 将直接关闭连接
- * * 当 type 为 "http" 时，Blackhole 会发回一个简单的 HTTP 403 数据包，然后关闭连接
- */
+* * When type is "none" (default value), Blackhole will close the connection directly
+* * When type is "http", Blackhole will send back a simple HTTP 403 packet, then close the connection
+*/
 const enum BLACKHOLE_RESPONSE {
     none = "none",
     http = "http"
 }
 
 /**
- * Blackhole（黑洞）是一个出站数据协议，它会阻碍所有数据的出站
- * 配合路由（Routing）一起使用，可以达到禁止访问某些网站的效果
- */
+* Blackhole is an outbound data protocol that blocks all data outbound
+* Used together with routing (Routing), can achieve the effect of prohibiting access to certain websites
+*/
 class BlackholeOutboundObject {
     /**
-     * 配置黑洞的响应数据
-     * 
-     * Blackhole 会在收到待转发数据之后，发送指定的响应数据，然后关闭连接
-     * 
-     * 待转发的数据将被丢弃。如不指定此项，Blackhole 将直接关闭连接
+     * Configure the response data of the black hole
+     *
+     * Blackhole will send the specified response data after receiving the data to be forwarded, and then close the connection
+     *
+     * Data to be forwarded will be discarded. If this is not specified, Blackhole will close the connection directly
      */
     response: {type: BLACKHOLE_RESPONSE } = {
         type: BLACKHOLE_RESPONSE.none
