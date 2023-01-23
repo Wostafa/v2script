e6edc8a556e2f5b3d71b459cea8694d849923e19
Wostafa <mostaphaaa@gmail.com>
2023-01-23 17:19:25 +0330

    comments translated

--- src/transport/tcp.ts
+++ src/transport/tcp.ts
@@ -55,25 +55,25 @@ class HttpHeaderObject {
 }
 
 /**
- * 当前连接的 TCP 配置，仅当此连接使用 TCP 时有效
- */
+* The TCP configuration of the current connection, only valid when this connection uses TCP
+*/
 class TcpObject {
     /**
-     * v4.27.1+，仅用于 inbound，是否接收 PROXY protocol
-     * 
-     * 填写 true 时，最底层 TCP 连接建立后，请求方必须先发送 PROXY protocol v1 或 v2，否则连接会被关闭
-     * 
-     * [PROXY protocol](https://www.haproxy.org/download/2.2/doc/proxy-protocol.txt)专用于传递请求的真实来源 IP 和端口，
-     * 若你不了解它，请先忽略该项
-     * 
-     * 常见的反代软件（如 HAProxy、Nginx）都可以配置发送它，VLESS fallbacks xver 也可以发送它
+     * v4.27.1+, only for inbound, whether to receive PROXY protocol
+     *
+     * When filling in true, after the bottom TCP connection is established, the requester must first send PROXY protocol v1 or v2, otherwise the connection will be closed
+     *
+     * [PROXY protocol](https://www.haproxy.org/download/2.2/doc/proxy-protocol.txt) is dedicated to passing the real source IP and port of the request,
+     * If you don't understand it, please ignore this item
+     *
+     * Common anti-generation software (such as HAProxy, Nginx) can be configured to send it, and VLESS fallbacks xver can also send it
      */
     acceptProxyProtocol: boolean = false;
 
     /**
-     * 数据包头部伪装设置
-     * 
-     * HTTP 伪装无法被其它 HTTP 服务器（如 Nginx）分流，但可以被 VLESS fallbacks path 分流
+     * Packet header masquerading settings
+     *
+     * HTTP masquerading cannot be diverted by other HTTP servers (such as Nginx), but it can be diverted by VLESS fallbacks path
      */
     header: NoneHeaderObject | HttpHeaderObject = new NoneHeaderObject();
 }
