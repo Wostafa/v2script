451926d9e10f82908551bb74b0646811aefd828c
Wostafa <mostaphaaa@gmail.com>
2023-01-23 17:13:50 +0330

    comments translated

--- src/transport/domainsocket.ts
+++ src/transport/domainsocket.ts
@@ -1,25 +1,25 @@
 /**
- * Domain Socket 使用标准的 Unix domain socket 来传输数据
- * 
- * 它的优势是使用了操作系统内建的传输通道，而不会占用网络缓存。相比起本地环回网络（local loopback）来说，Domain socket 速度略快一些。
- * 
- * 目前仅可用于支持 Unix domain socket 的平台，如 Linux 和 macOS。在 Windows 10 Build 17036 前不可用。
- * 
- * 如果指定了 domain socket 作为传输方式，在入站出站代理中配置的端口和 IP 地址将会失效，所有的传输由 domain socket 取代
- */
+* Domain Socket uses standard Unix domain socket to transmit data
+*
+* Its advantage is that it uses the built-in transmission channel of the operating system without occupying the network cache. Compared with the local loopback network (local loopback), Domain socket is slightly faster.
+*
+* Currently only available on platforms that support Unix domain sockets, such as Linux and macOS. Not available prior to Windows 10 Build 17036.
+*
+* If domain socket is specified as the transmission method, the port and IP address configured in the inbound and outbound proxy will be invalid, and all transmissions will be replaced by domain socket
+*/
 export class DomainSocketObject {
-    /** 一个合法的文件路径。在运行 V2Ray 之前，这个文件必须不存在 */
+    /** A valid file path. This file must not exist before running V2Ray */
     path: string;
 
-    /** 是否为 abstract domain socket */
+    /** Is it an abstract domain socket */
     abstract: boolean = false;
 
-    /** abstract domain socket 是否带 padding */
+    /** Whether the abstract domain socket has padding */
     padding: boolean = false;
 
     /**
      * DomainSocketObject
-     * @param path 一个合法的文件路径
+     * @param path a legal file path
      */
     constructor(path: string) {
         this.path = path;
